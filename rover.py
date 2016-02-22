7032b7fb9f6b7f78572e1e3c910ec7ad3ac1be48
Jonathan Windridge <jonathan.windridge@gmail.com>
2016-02-22 08:29:35 +0000

    Fix compass_index test

--- rover.py
+++ rover.py
@@ -11,6 +11,11 @@ class Rover:
     def position(self):
         return self.x, self.y, self.direction
 
+    @property
+    def compass_index(self):
+        return next(i for i in range(0, len(self.compass)) if self.compass[i] == self.direction)
+
+
     def set_position(self, x, y, direction):
         self.x = x
         self.y = y
