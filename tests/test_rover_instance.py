1c0f0decd5bdcea3174cee650ba08fb427b67016
Jonathan Windridge <jonathan.windridge@gmail.com>
2016-02-22 08:22:53 +0000

    Add failing tests for rover forward movement

--- tests/test_rover_instance.py
+++ tests/test_rover_instance.py
@@ -16,3 +16,23 @@ class TestRover(TestCase):
     def test_rover_set_position(self):
         self.rover.set_position(4, 9, 'W')
         assert self.rover.position == (4, 9, 'W')
+
+    def test_rover_move_forward_north(self):
+        self.rover.set_position(0, 0, 'N')
+        self.rover.move('F')
+        assert self.rover.position == (0, 1, 'N')
+
+    def test_rover_move_forward_south(self):
+        self.rover.set_position(0, 1, 'S')
+        self.rover.move('F')
+        assert self.rover.position == (0, 0, 'S')
+
+    def test_rover_move_forward_east(self):
+        self.rover.set_position(0, 0, 'E')
+        self.rover.move('F')
+        assert self.rover.position == (1, 0, 'E')
+
+    def test_rover_move_forward_west(self):
+        self.rover.set_position(1, 0, 'W')
+        self.rover.move('F')
+        assert self.rover.position == (0, 0, 'W')
